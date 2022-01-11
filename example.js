6e9e2e09dcb74425775059dd098f3d00497b5a95
Enrique Coscarelli <enriquecoscarelli@Enriques-MacBook-Air.local>
2022-01-11 10:51:30 -0300

    First commit

---
+++ example.js
@@ -0,0 +1,147 @@
+// DATA
+
+// let num = 12.5;
+// let num1 = 12;
+// let strDouble = "this is a string";
+// let strSingle = 'this is a string';
+// let strBacktics = `this is a string`;
+// let question = '12'
+// questtion = "13"
+
+// console.log("question", question)
+// console.log("questtion", questtion)
+
+// questtion = "14"
+
+// console.log("questtion", questtion)
+
+// let boo = true
+
+// let idk = ''
+// let idke
+// let martinsQuestion = 'undefined'
+
+
+// console.log(typeof idke)
+
+// console.log(typeof martinsQuestion)
+
+// MATH
+
+// let numNum = 12
+
+// console.log(numNum % 8)
+
+// CONDITIONS
+
+// let today = 29
+
+// if (today > 10 || today < 23){
+//   console.log("JavaScript")
+// } else if (today > 23 && today < 30){
+//   console.log("React")
+// } else if (today < 10) {
+//   console.log("HTML, CSS and GIT")
+// } else {
+//   console.log("You are not learning JS")
+// }
+
+// TERNARY
+
+// let payed = true
+
+// let termsAndConditions = payed ? "welcome" : "You need to go to the counter"
+
+// console.log(termsAndConditions)
+
+// SWITCH
+
+// let day = 
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday")
+//     break;
+//   case 2:
+//     console.log("Tuesday")
+//     break;
+//   case 3:
+//     console.log("Wednesday")
+//     break;
+//   case 4:
+//     console.log("Thurdays")
+//     break;
+//   case 5:
+//     console.log("Friday")
+//     break;
+//   case 6:
+//     console.log("Saturday")
+//     break;
+//   case 7:
+//     console.log("Sunday")
+//     break;
+//   default:
+//     console.log("The week only has 7 days, sorry")
+// }
+
+// ARRAYS
+
+// let arr = ['martin', "guillermo", 12, true, undefined, 'anna']
+
+// console.log(arr[0])
+// console.log(arr[5])
+// console.log(arr[3])
+// console.log(arr.length)
+// console.log(arr[arr.length-1])
+
+let prices = [12.5, 14, 128, 0.5]
+
+// for (let i = 0; i < prices.length; i++){
+//   console.log(prices[i])
+// }
+
+
+// console.log("first loop is done")
+
+// for (let i = 3; i > -1; i--){
+//   console.log(prices[i])
+// }
+
+
+
+
+// console.log("forloop is over")
+
+// for (let i = 0; i < prices.length; i++){
+//   console.log(prices[i])
+//   prices[i] += 0.5
+//   console.log(prices[i])
+// }
+
+
+// console.log("first loop is done")
+
+// ARRAY METHODS
+
+// prices.forEach(element => console.log(element+1))
+
+// console.log(prices)
+
+// let newArray = prices.map(element => element+1)
+
+// console.log(newArray)
+// console.log(prices)
+
+// FUNCTIONS
+
+// function name() {
+//   console.log("hello")
+// }
+
+// name()
+
+// function greeter(guest, course){
+//   console.log("Hello "+guest+".Welcome to your "+course+" course")
+// }
+
+// greeter("Muhammad", "FSWD")
